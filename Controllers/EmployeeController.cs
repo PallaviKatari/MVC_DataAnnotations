8641243da4f9840b8b5246a09fba41f2ec7af112
PallaviKatari <pallavikatari@gmail.com>
2022-03-30 18:12:02 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_DataAnnotations.Models;
+
+namespace MVC_DataAnnotations.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
